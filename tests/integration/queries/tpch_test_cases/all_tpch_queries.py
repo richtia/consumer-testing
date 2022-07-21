f03b7cf2021282ad2f5d65fb40a88350b44ee109
Richard Tia <richie.tia@gmail.com>
2022-07-21 08:39:00 -0700

    add empty json to plans

--- tests/integration/queries/tpch_test_cases/all_tpch_queries.py
+++ tests/integration/queries/tpch_test_cases/all_tpch_queries.py
@@ -3,7 +3,17 @@ from .query_2 import TESTCASE as q2
 from .query_3 import TESTCASE as q3
 from .query_4 import TESTCASE as q4
 from .query_5 import TESTCASE as q5
+from .query_6 import TESTCASE as q6
+from .query_7 import TESTCASE as q7
+from .query_8 import TESTCASE as q8
+from .query_9 import TESTCASE as q9
+from .query_10 import TESTCASE as q10
+from .query_11 import TESTCASE as q11
+from .query_12 import TESTCASE as q12
+from .query_13 import TESTCASE as q13
+from .query_14 import TESTCASE as q14
+from .query_15 import TESTCASE as q15
 
 TPCH_QUERY_TESTS = (
-        q1 + q2 + q3 + q4 + q5
+        q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10 + q11 + q12 + q13 + q14 + q15
 )
